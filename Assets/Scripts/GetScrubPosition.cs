baff878ab8ad73828a93523ff28039af05c674e2
Riley Florence <rileyflorence@gmail.com>
2018-09-29 10:55:55 -0600

    Getting Set up

---
+++ Assets/Scripts/GetScrubPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetScrubPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate()
+
+    {
+        Vector3 dwn = transform.TransformDirection(Vector3.down);
+        Debug.DrawRay(transform.position, dwn * 1, Color.green);
+
+        if (Physics.Raycast(transform.position, dwn, 1))
+            print("We found it!");
+
+    }
+}
