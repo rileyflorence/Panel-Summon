298e27a18cea88e5d9c36587ec7ccaf5287d88b7
Riley Florence <rileyflorence@gmail.com>
2018-09-29 21:32:40 -0600

    All set up. Just need leap motion.

--- Assets/Scripts/GetScrubPosition.cs
+++ Assets/Scripts/GetScrubPosition.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GetScrubPosition : MonoBehaviour {
+    public float scrubPosition;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	void FixedUpdate()
+	void Update()
     {
+        
         int layerMask = 1 << 9;
-        Vector3 dwn = transform.TransformDirection(Vector3.down);
-        Debug.DrawRay(transform.position, dwn * 1, Color.green);
+        Vector3 _relativeLoc;
+        RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, dwn, 1, layerMask))
-            print("We found it!");
+        Vector3 dwn = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, dwn, out hit, 1, layerMask))
+        {
+            Debug.DrawRay(transform.position, dwn * 1, Color.green);
+            _relativeLoc = hit.transform.InverseTransformPoint(hit.point);
+            scrubPosition = _relativeLoc.x + 0.5f;
+        
+
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, dwn * 1, Color.red);
+        }
 
     }
 }
