7df15bd4e5b583b4f1093042c823af8bc2d8ab4c
Riley Florence <rileyflorence@gmail.com>
2018-10-01 21:53:22 -0600

    Commented the code.
    Put some comments in the code. Removed some unused stuff.

--- Assets/Scripts/GetScrubPosition.cs
+++ Assets/Scripts/GetScrubPosition.cs
@@ -5,30 +5,34 @@ using UnityEngine;
 public class GetScrubPosition : MonoBehaviour {
     public float scrubPosition;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	void Update()
     {
         
+        //Bit shifting the layer mask number. Seems weird, but whatever.
         int layerMask = 1 << 9;
         Vector3 _relativeLoc;
         RaycastHit hit;
 
         Vector3 dwn = Vector3.down;
 
+        //Casts a ray down from the associated game object.
         if (Physics.Raycast(transform.position, dwn, out hit, 1, layerMask))
         {
+            //draw a line in the editor. Green if it hits the correct layer.
             Debug.DrawRay(transform.position, dwn * 1, Color.green);
+
+            //Gets the hit location and then turns it's location relative to the hit object instead of being in worldspace.
             _relativeLoc = hit.transform.InverseTransformPoint(hit.point);
+
+            //Get just the X value, then add 0.5 because the relative location starts at the center, but I want it to be from the edge.
             scrubPosition = _relativeLoc.x + 0.5f;
         
 
         }
         else
         {
+            //Draw a debug ray in the editor that's red if it doesn't hit a valid layer.
             Debug.DrawRay(transform.position, dwn * 1, Color.red);
         }
 
--- Assets/Scripts/animController.cs
+++ Assets/Scripts/animController.cs
@@ -6,20 +6,20 @@ public class animController : MonoBehaviour {
 
     Animator animator;
 
+    //I created an empty that's where the palm is of the right hand. This is a ref to that object.
     [SerializeField]
     GameObject hand;
 
     GetScrubPosition scrubPos;
 
-	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         scrubPos = hand.GetComponent<GetScrubPosition>();
     
 	}
 	
-	// Update is called once per frame
 	void Update () {
+        //Sets the animation frame.
         animator.Play("OpenMenu_ANIM", 0, scrubPos.scrubPosition);
 	}
 }
